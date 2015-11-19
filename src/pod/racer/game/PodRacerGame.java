f2a45dd9a1664cee75b13016eefd7bdd46fe9505
TheKikBoxer <watercraft.talk@gmail.com>
2015-11-19 09:11:51 -0800

    Project created

---
+++ src/pod/racer/game/PodRacerGame.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pod.racer.game;
+
+import environment.ApplicationStarter;
+import environment.Environment;
+
+/**
+ *
+ * @author Liam
+ */
+public class PodRacerGame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ApplicationStarter.run("Pod Race", new Tatooine()); }
+
+    
+    
+}
